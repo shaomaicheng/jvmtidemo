24e82f0f0a1cfbce83f44e2edd6050093f422a8f
chenglei <devchenglei@gmail.com>
2024-02-19 00:17:11 +0800

    添加代码，jvmti的回调里面反向调用java的上报方法

--- mylibrary/src/main/cpp/jnicall.cpp
+++ mylibrary/src/main/cpp/jnicall.cpp
@@ -21,6 +21,10 @@ jclass jvmtiFindClass(JNIEnv *env, jvmtiEnv *jvmtiEnv, jstring className) {
     jvmtiEnv->GetThreadInfo(currentThread, &info);
     jobject classLoader = info.context_class_loader;
 
+    __android_log_print(ANDROID_LOG_ERROR, "chenglei_jni",
+                        "classclazz is null: %d,forNameMethodId is null:%d,,classLoader is null:%d,threadname:%s",
+                        classClazz ==nullptr, forNameMethodId ==nullptr, classLoader ==nullptr,info.name);
+
     jclass targetClazz = static_cast<jclass>(env->CallStaticObjectMethod(classClazz,
                                                                          forNameMethodId,
                                                                          className, JNI_FALSE,
@@ -28,23 +32,21 @@ jclass jvmtiFindClass(JNIEnv *env, jvmtiEnv *jvmtiEnv, jstring className) {
     return targetClazz;
 }
 
-JNIEnv *detectJniEnv(JavaVM* g_JavaVM) {
+JNIEnv *detectJniEnv(JavaVM *g_JavaVM) {
     JNIEnv *env;
     //获取当前native线程是否有没有被附加到jvm环境中
     int getEnvStat = g_JavaVM->GetEnv((void **) &env, JNI_VERSION_1_6);
-    __android_log_print(ANDROID_LOG_ERROR,"chenglei_jni", "GetEnv: %d", getEnvStat);
+    __android_log_print(ANDROID_LOG_ERROR, "chenglei_jni", "GetEnv: %d", getEnvStat);
     return env;
 }
 
 
-
 void jvmtiReport(JNIEnv *jni_env, jvmtiEnv *jvmtiEnv, JavaVM *vm, int type, const char *log) {
-    jboolean thread= false;
-    if (jni_env == nullptr){
-        thread= true;
+    jboolean thread = false;
+    if (jni_env == nullptr) {
+        thread = true;
     }
-    if (thread)
-    {
+    if (thread) {
         vm->AttachCurrentThread(&jni_env, nullptr);
     }
     jstring name = jni_env->NewStringUTF("com.example.mylibrary.JvmtiReport");
@@ -58,13 +60,11 @@ void jvmtiReport(JNIEnv *jni_env, jvmtiEnv *jvmtiEnv, JavaVM *vm, int type, cons
                                                           "(ILjava/lang/String;)V");
     jstring logParam = jni_env->NewStringUTF(log);
     jni_env->CallStaticVoidMethod(reportClazz, reportMethodId, type, logParam);
-    if (thread)
-    {
+    if (thread) {
         vm->DetachCurrentThread();
     }
 }
 
-void jvmtiReportWithP( JvmtiReportParam param)
-{
-    jvmtiReport(nullptr, param.jvmtiEnv, param.vm, param.type,param.log);
+void jvmtiReportWithP(JvmtiReportParam param) {
+    jvmtiReport(nullptr, param.jvmtiEnv, param.vm, param.type, param.log);
 }
